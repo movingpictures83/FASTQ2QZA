0f765feba21210a519607d2039c2f626bc95b7aa
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-08-15 21:08:15 -0400

    Fixed conda

--- FASTQ2QZAPlugin.cpp
+++ FASTQ2QZAPlugin.cpp
@@ -19,9 +19,11 @@ void FASTQ2QZAPlugin::run() {
 }
 
 void FASTQ2QZAPlugin::output(std::string file) { 
-   std::string command = "eval \"$(conda shell.bash hook)\"; ";
-   command += "conda activate qiime2-2020.11; ";
-   command += "qiime tools import --type \'SampleData[PairedEndSequencesWithQuality]\' --input-path "+std::string(PluginManager::prefix())+"/"+parameters["manifest"]+" --output-path "+file+" --input-format "+parameters["format"]+"; conda deactivate";
+   std::string command = "export OLDPATH=${PATH}; ";
+   command += "export PATH=${CONDA_HOME}/bin/:${PATH}; ";
+   command += "eval \"$(conda shell.bash hook)\"; ";
+   command += "conda activate qiime2-2021.4; ";
+   command += "qiime tools import --type \'SampleData[PairedEndSequencesWithQuality]\' --input-path "+std::string(PluginManager::prefix())+"/"+parameters["manifest"]+" --output-path "+file+" --input-format "+parameters["format"]+"; ";
 
 //qiime tools import \
   --type 'SampleData[PairedEndSequencesWithQuality]' \
@@ -29,6 +31,11 @@ void FASTQ2QZAPlugin::output(std::string file) {
   --output-path AlphaFinal1-paired-end-demux.qza \
   --input-format PairedEndFastqManifestPhred33V2
  //std::cout << command << std::endl;
+   command += "conda deactivate; ";
+   command += "conda deactivate; ";
+   command += "export PATH=${OLDPATH}";
+ std::cout << command << std::endl;
+
 
  system(command.c_str());
 }
